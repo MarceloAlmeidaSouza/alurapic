d61c3ad34ff10627a329dce5b9fe3408fc5dfcc0
Marcelo Almeida <marcelo.souza@cejam.org.br>
2021-09-09 01:01:59 -0300

    Alura curso

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="corpo">
-    <router-view></router-view>
+   <meu-menu :rotas="routes"/>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import routes from './routes.js';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
+  data(){
+    return {
+      routes
+    }
+  }
 }
 </script>
 
@@ -14,5 +29,12 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+  .pagina-enter, .pagina-fade-leave-active {
+    opacity: 0
+  }
+
+  .pagina-enter-active, .pagina-fade-leave-active {
+    transition: opacity .4s
   }
 </style>
