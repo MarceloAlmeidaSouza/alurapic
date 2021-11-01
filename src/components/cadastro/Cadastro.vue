56700db9abd524c3e265670f2b72570db7b848a0
Marcelo Almeida <marcelo.souza@cejam.org.br>
2021-11-01 01:17:26 -0300

    Curso alura, vuejs

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -10,13 +10,17 @@
     <form @submit.prevent="grava">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input name="titulo" v-validate data-vv-rules="required" id="titulo" autocomplete="off" v-model="foto.titulo">
-        <span v-show="errors.has('titulo')">ERRO</span>
+        <input name="titulo" v-model="foto.titulo" id="titulo" autocomplete="off" 
+        v-validate data-vv-rules="required|min:3|max:30"  data-vv-as="título" >
+         <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        
+         <input name="url" v-model="foto.url" id="url" autocomplete="off"
+        v-validate data-vv-rules="required">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -54,9 +58,20 @@ export default {
   },
   methods:{
     grava(){
-      this.service
-        .cadastra(this.foto)
-        .then(() => {if(this.id) this.$router.push({ name: 'home'});this.foto = new Foto();}, err => console.log(err));
+      this.$validator
+          .validateAll()
+          .then(success => {
+            if(success) {
+
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if(this.id) this.$router.push({ name: 'home'});
+                  this.foto = new Foto()
+                }, 
+                err => console.log(err));
+            }
+        });
     }
   }, 
 
@@ -95,5 +110,7 @@ export default {
   .centralizado {
     text-align: center;
   }
-
+  .erro {
+    color: red;
+  }
 </style>
